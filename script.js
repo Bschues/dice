e5621888ac140fb4fceb64a47f2dbacb4c6a8cc3
bschue <bschuess3@gmail.com>
2018-05-01 02:10:09 -0400

    second commit

--- script.js
+++ script.js
@@ -10,10 +10,9 @@ function dice2(faces) {
 
     return randomnumber2;
 }
-
+//Created two funtions that generate a random number
 
 function rollresults() {
-    //var rollsumar = [];
 
     let count = [];
     for (i = 1; i <= 1000; i++) {
@@ -25,13 +24,19 @@ function rollresults() {
         }
     }
     for (i = 2; i < count.length; i++) {
+        //count[];
         let box = document.getElementById("div");
         let p = document.createElement("p");
         let stuff = document.createTextNode(i.toString() + " : " + count[i])
         p.appendChild(stuff);
         box.appendChild(p);
-
-    }
+        document.getElementById("p")
+        p.style.backgroundColor = "honeydew";
+        p.style.width = count[i] + "px";
+        box.style.border = 2 + "px";
+        box.style.border = "dotted";
+        box.style.borderColor = "honeydew";
+        }
 
 
 
